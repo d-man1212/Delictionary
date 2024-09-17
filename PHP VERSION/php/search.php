d8ea385e0b33f78d13225025b0fdd7dd82f4f755
d-man1212 <itsmedharshan.shankar@gmail.com>
2024-09-18 01:28:54 +0530

    Revert "PHP Version Added"
    This reverts commit d74bc172ad25657f19ee83390b9ad63848a30062.

--- PHP VERSION/php/search.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'dbsetup.php';
-$query = $conn->real_escape_string($_GET['search']);
-$app_id = '<EDAMAM APP ID>';
-$app_key = '<EDAMAM APP KEY>';
-$url = "https://api.edamam.com/api/recipes/v2?type=any&beta=true&q=${query}&app_id=${app_id}&app_key=${app_key}";
-$response = file_get_contents($url);
-echo $response;
-?>
